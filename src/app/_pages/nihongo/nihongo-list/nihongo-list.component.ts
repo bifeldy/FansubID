735ec70547433fd9fafdc5c39c43e790606bb417
Bifeldy <bias.astho@gmail.com>
2020-12-25 02:29:32 +0900

    ✔ 💩

--- src/app/_pages/nihongo/nihongo-list/nihongo-list.component.ts
+++ src/app/_pages/nihongo/nihongo-list/nihongo-list.component.ts
@@ -51,21 +51,19 @@ export class NihongoListComponent implements OnInit {
           <img class="col-12 col-md-6" src="/assets/img/nihongo/roadmap-1.jpg" />
           <img class="col-12 col-md-6" src="/assets/img/nihongo/roadmap-2.jpg" />
         </div>
-        <p class="mat-caption">Sumber gambar dan informasi: <a class="text-decoration-none" href="https://kawakawalearningstudio.com/all/roadmap-for-the-jlpt">KawaKawa</a></p>
-        <p>“JLPT” adalah singkatan dari Japanese-Language Proficiency Test (Tes Kemampuan Bahasa Jepang). JLPT adalah salah satu ujian kecakapan bahasa Jepang internasional yang paling terkenal. Ini memiliki lima level, dengan level lima (secara resmi disebut N5) menjadi yang termudah dan level satu (N1) menjadi yang paling sulit. JLPT dulu memiliki empat level tetapi sekarang dibagi menjadi lima (N5, N4, N3, N2, dan N1). Level mana yang ingin diambil dibebaskan untuk memilih. Contoh pertanyaan dapat diakses pada halaman <a class="text-decoration-none" href="http://www.jlpt.jp/e/samples/sample09.html">Website JLPT</a>.</p>
+        <p class="mat-caption">Sumber gambar dan informasi: <a target="_blank" class="text-decoration-none" href="https://kawakawalearningstudio.com/all/roadmap-for-the-jlpt">KawaKawa</a></p>
+        <p>“JLPT” adalah singkatan dari Japanese-Language Proficiency Test (Tes Kemampuan Bahasa Jepang). JLPT adalah salah satu ujian kecakapan bahasa Jepang internasional yang paling terkenal. Ini memiliki lima level, dengan level lima (secara resmi disebut N5) menjadi yang termudah dan level satu (N1) menjadi yang paling sulit.</p>
+        <p>JLPT dulu memiliki empat level tetapi sekarang dibagi menjadi lima (N5, N4, N3, N2, dan N1). Level mana yang ingin diambil dibebaskan untuk memilih. Contoh pertanyaan dapat diakses pada halaman <a target="_blank" class="text-decoration-none" href="http://www.jlpt.jp/e/samples/sample09.html">New Japanese-Language Proficiency Test Sample Questions</a>.</p>
         <ul>
-          <li>Untuk lulus N5, perlu mengetahui sekitar 100 kanji dan sekitar 800 kosakata.</li>
-          <li>Untuk lulus N4, perlu mengetahui sekitar 300 kanji dan sekitar 1.500 kosakata.</li>
-          <li>Untuk lulus N3, perlu mengetahui sekitar 650 kanji dan sekitar 3.700 kosakata.</li>
-          <li>Untuk lulus N2, perlu mengetahui sekitar 1.000 kanji dan sekitar 6.000 kosakata.</li>
-          <li>Untuk lulus N1, perlu mengetahui sekitar 2.000 kanji dan sekitar 10.000 kosakata.</li>
+          <li>Untuk lulus <a target="_blank" class="text-success text-decoration-none">N5</a>, perlu mengetahui sekitar <a target="_blank" class="text-success text-decoration-none">100</a> kanji dan sekitar <a target="_blank" class="text-success text-decoration-none">800</a> kosakata.</li>
+          <li>Untuk lulus <a target="_blank" class="text-success text-decoration-none">N4</a>, perlu mengetahui sekitar <a target="_blank" class="text-success text-decoration-none">300</a> kanji dan sekitar <a target="_blank" class="text-success text-decoration-none">1.500</a> kosakata.</li>
+          <li>Untuk lulus <a target="_blank" class="text-success text-decoration-none">N3</a>, perlu mengetahui sekitar <a target="_blank" class="text-success text-decoration-none">650</a> kanji dan sekitar <a target="_blank" class="text-success text-decoration-none">3.700</a> kosakata.</li>
+          <li>Untuk lulus <a target="_blank" class="text-success text-decoration-none">N2</a>, perlu mengetahui sekitar <a target="_blank" class="text-success text-decoration-none">1.000</a> kanji dan sekitar <a target="_blank" class="text-success text-decoration-none">6.000</a> kosakata.</li>
+          <li>Untuk lulus <a target="_blank" class="text-success text-decoration-none">N1</a>, perlu mengetahui sekitar <a target="_blank" class="text-success text-decoration-none">2.000</a> kanji dan sekitar <a target="_blank" class="text-success text-decoration-none">10.000</a> kosakata.</li>
         </ul>
-        <div class="row">
-          <img class="col-12 col-md-6" src="/assets/img/nihongo/roadmap-3.jpg" />
-          <img class="col-12 col-md-6" src="/assets/img/nihongo/roadmap-4.jpg" />
-        </div>
-        <p class="mat-caption">Sumber gambar dan informasi: <a class="text-decoration-none" href="https://japaneselanguageinstitute.wordpress.com/2018/05/15/scoring-section-in-jlpt-exam-minimum-passing-marks-required-for-each-level/">Nihonkai</a></p>
-        <p class="mat-body-strong mat-body-2 font-weight-bold">N5 dan N4 adalah level termudah untuk diselesaikan dalam tes tetapi jika ingin menyelesaikan level yang lebih tinggi seperti N3, N2 dan N1 maka harus menghadiri pusat pembinaan di mana mereka menyediakan bahan pelajaran yang baik dan memiliki pelatih berpengalaman akan membantu belajar bahasa jepang dengan mudah.</p>
+        <p>Untuk informasi skoring & penilaian dapat dilihat pada <a target="_blank" class="text-decoration-none" href="https://www.jlpt.jp/e/guideline/results.html">Scoring Sections, Pass or Fail, Score Report</a>.</p>
+        <p class="text-danger mat-body-strong mat-body-2 font-weight-bold">N5 dan N4 adalah level termudah untuk diselesaikan dalam tes tetapi jika ingin menyelesaikan level yang lebih tinggi seperti N3, N2 dan N1 maka harus menghadiri pusat pembinaan di mana mereka menyediakan bahan pelajaran yang baik dan memiliki pelatih berpengalaman akan membantu belajar bahasa jepang dengan mudah.</p>
+        <p>Informasi lebih lanjut, silahkan kunjungi <a target="_blank" class="text-decoration-none" href="https://www.jlpt.jp/e/index.html">https://www.jlpt.jp/e/index.html</a>.</p>
       `
     },
     {
