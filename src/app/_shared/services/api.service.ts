9d61bc9cad7ca2ba961c52bc034deb235b094af4
Bifeldy <bias.astho@gmail.com>
2021-08-08 18:15:32 +0900

    ✔ Timeout Limit

--- src/app/_shared/services/api.service.ts
+++ src/app/_shared/services/api.service.ts
@@ -37,22 +37,22 @@ export class ApiService {
     }
   }
 
-  getData(path: string, options = {}, timedOut = 10000, retryCount = 3): Observable<any> {
+  getData(path: string, options = {}, timedOut = 20000, retryCount = 3): Observable<any> {
     this.gs.log('[API_GET]', path);
     return this.http.get(this.HTTP_REQ_URL(path), options).pipe(
-      catchError(err => throwError(err)),
+      catchError(err => throwError(() => err)),
       map(res => res), timeout(timedOut), retry(retryCount)
     );
   }
 
-  postData(path: string, model = {}, multipart = false, options = {}, timedOut = 10000): Observable<any> {
+  postData(path: string, model = {}, multipart = false, options = {}, timedOut = 15000): Observable<any> {
     this.gs.log('[API_POST]', path);
     let body = model;
     if (multipart) {
       body = this.prepareFormData(model);
     }
     return this.http.post(this.HTTP_REQ_URL(path), body, options).pipe(
-      catchError(err => throwError(err)),
+      catchError(err => throwError(() => err)),
       map(res => res), timeout(timedOut)
     );
   }
@@ -64,7 +64,7 @@ export class ApiService {
       body = this.prepareFormData(model);
     }
     return this.http.put(this.HTTP_REQ_URL(path), body, options).pipe(
-      catchError(err => throwError(err)),
+      catchError(err => throwError(() => err)),
       map(res => res), timeout(timedOut)
     );
   }
@@ -76,15 +76,15 @@ export class ApiService {
       body = this.prepareFormData(model);
     }
     return this.http.patch(this.HTTP_REQ_URL(path), body, options).pipe(
-      catchError(err => throwError(err)),
+      catchError(err => throwError(() => err)),
       map(res => res), timeout(timedOut)
     );
   }
 
-  deleteData(path: string, timedOut = 10000): Observable<any> {
+  deleteData(path: string, timedOut = 5000): Observable<any> {
     this.gs.log('[API_DELETE]', path);
     return this.http.delete(this.HTTP_REQ_URL(path)).pipe(
-      catchError(err => throwError(err)),
+      catchError(err => throwError(() => err)),
       map(res => res), timeout(timedOut)
     );
   }
