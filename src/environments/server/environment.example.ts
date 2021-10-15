bd8d857f18fc8c923776dcf07aa6bbc458dae31f
Bifeldy <bias.astho@gmail.com>
2021-10-16 08:01:14 +0900

    ✔ Using Old Discord Channel

--- src/environments/server/environment.example.ts
+++ src/environments/server/environment.example.ts
@@ -92,7 +92,7 @@ export const environment = {
   discordClientId: '789831990433153034',
   discordClientSecret: '',
   laboratoryRatsRoleId: '479208130534703108',
-  discordBotChannelEventId: '859977432282824714',
+  discordBotChannelEventId: '790158935045701652',
   discordBotChannelBotId: '426384107795251206',
   discordBotLoginToken: '',
   nodeJsXhrHeader: {
