fb6cf2b640104423cf51ae9edcb17c91df9eeb85
Bifeldy <bias.astho@gmail.com>
2021-10-30 22:38:31 +0900

    ✔ 🤷‍♀️

--- src/app/_shared/components/material-table/material-table.module.ts
+++ src/app/_shared/components/material-table/material-table.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { MaterialTableComponent } from './material-table.component';
+
 import { SharedMaterialModule } from '../../helpers/shared-material.module';
 
-import { MaterialTableComponent } from './material-table.component';
 import { MaterialChipModule } from '../material-chip/material-chip.module';
+
 import { CustomPipeModule } from '../../pipes/custom-pipe.module';
 
 @NgModule({
