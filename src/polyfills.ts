876e8f06d2a054a3fedc266834f24843a70fbbdc
Bifeldy <bias.astho@gmail.com>
2021-11-01 21:57:36 +0900

    ✔ 🤔

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,4 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+(window as any).global = window;
