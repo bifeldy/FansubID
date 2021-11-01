394aee5bcdf15d4862509ebf592fe530ecb133b1
Bifeldy <bias.astho@gmail.com>
2021-11-01 19:10:17 +0900

    ✔ Remove Unused Code

--- src/app/_shared/services/torrent.service.ts
+++ src/app/_shared/services/torrent.service.ts
@@ -60,8 +60,7 @@ export class TorrentService {
   public tableDataRow: any[] = [];
 
   error = null;
-  errorTimeoutId = null;
-  errorTimeoutCooldown = 60000;
+  refCallback = null;
 
   constructor(
     public gs: GlobalService,
@@ -76,36 +75,21 @@ export class TorrentService {
   handleClient(): void {
     this.client.on('torrent', torrent => {
       this.gs.log('[TORRENT_CLIENT_ADD_TORRENT_FILE]', torrent);
+      this.toast.info(torrent.infoHash, 'Woaw, Antrian Baru!');
+      this.tableDataRow.push(torrent);
+      this.refCallback(null, torrent);
     });
     this.client.on('error', err => {
       this.gs.log('[TORRENT_CLIENT_ERROR]', err);
       this.toast.error(err.toString(), 'Whoops! Error.');
       this.error = err;
+      this.refCallback(this.error, null);
     });
   }
 
   handleTorent(torrent: Torrent, callback): void {
-    torrent.on('infoHash', () => {
-      this.gs.log('[TORRENT_FILE_INFOHASH]', torrent.infoHash);
-    });
-    torrent.on('metadata', () => {
-      this.gs.log('[TORRENT_FILE_METADATA]');
-    });
-    torrent.on('ready', () => {
-      this.gs.log('[TORRENT_FILE_READY]');
-    });
     torrent.on('done', () => {
-      this.gs.log('[TORRENT_FILE_DONE]');
       this.toast.success(`Ada Torrent Yang Sudah Selesai Di Download`, 'Yeay, Selesai!');
-    });
-    torrent.on('download', bytes => {
-      this.gs.log('[TORRENT_FILE_DOWNLOAD]', bytes);
-    });
-    torrent.on('upload', bytes => {
-      this.gs.log('[TORRENT_FILE_UPLOAD]', bytes);
-    });
-    torrent.on('noPeers', announceType => {
-      this.gs.log('[TORRENT_FILE_NOPEERS]', announceType);
     });
     torrent.on('warning', err => {
       this.gs.log('[TORRENT_FILE_WARNING]', err);
@@ -116,63 +100,38 @@ export class TorrentService {
       this.toast.error(err.toString(), 'Whoops! Error.');
       callback(err, null);
     });
-    torrent.on('wire', (wire, addr) => {
-      this.gs.log('[TORRENT_FILE_WIRE_ADDR]', addr);
-      this.handleWire(wire);
-    });
-  }
-
-  handleWire(wire): void {
     //
     // TODO :: Network Graph Peer Connections
     // https://github.com/webtorrent/bittorrent-protocol
     //
-    this.gs.log('[TORRENT_WIRE_CONNECT]', wire.peerId);
-    wire.once('close', () => {
-      this.gs.log('[TORRENT_WIRE_DISCONNECT]', wire.peerId);
-    })
+    torrent.on('wire', (wire) => {
+      this.gs.log('[TORRENT_WIRE_CONNECT]', wire.peerId);
+      wire.once('close', () => {
+        this.gs.log('[TORRENT_WIRE_DISCONNECT]', wire.peerId);
+      });
+    });
   }
 
   downloadFiles(magnetHash: string, callback): void {
     this.gs.log('[TORRENT_CLIENT_QUEUE_DOWNLOAD]', magnetHash);
+    this.refCallback = callback;
     this.client.add(magnetHash, this.torrentOptions, torrent => {
-      this.tableDataRow.push(torrent);
+      this.gs.log('[TORRENT_FILE_DOWNLOAD_READY]', torrent);
       this.handleTorent(torrent, callback);
-      clearTimeout(this.errorTimeoutId);
-      callback(null, torrent);
     });
-    this.checkError(magnetHash, callback);
   }
 
   uploadFiles(userInput: any, files: Array<any>, callback): void {
     this.gs.log('[TORRENT_CLIENT_QUEUE_UPLOAD]', files);
+    this.refCallback = callback;
     this.client.seed(files, ({
       ...this.torrentOptions,
       name: userInput.torrentBerkasName.inputText,
       comment: userInput.torrentKomentar.inputText,
       createdBy: `${environment.siteName} WebTorrent (｡>﹏<｡)`
     } as any), torrent => {
-      this.gs.log('[TORRENT_FILE_SEED_SUCCESS]', torrent);
-      this.tableDataRow.push(torrent);
-      clearTimeout(this.errorTimeoutId);
-      callback(null, torrent);
+      this.gs.log('[TORRENT_FILE_SEED_READY]', torrent);
     });
-    this.checkError(null, callback);
-  }
-
-  checkError(torrentId = null, callback): void {
-    this.errorTimeoutId = setTimeout(() => {
-      this.gs.log('[TORRENT_FILE_TIMEOUT]');
-      callback(this.error, null);
-      if (this.error) {
-        this.error = null;
-        this.removeTorrent(torrentId, err => {
-          if (!err) {
-            this.gs.log('[TORRENT_FILE_REMOVE_SUCCESS]', torrentId);
-          }
-        });
-      }
-    }, this.errorTimeoutCooldown);
   }
 
   removeTorrent(torrentId, callback): void {
