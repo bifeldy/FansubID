ee0e3d7b4b12f1202ab0be346a5bd8531cacc24b
Bifeldy <bias.astho@gmail.com>
2022-07-17 01:03:31 +0900

    ✔🎉 Fix Routing ✨

--- src/app/_shared/components/leaderboard/leaderboard.module.ts
+++ src/app/_shared/components/leaderboard/leaderboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { SharedMaterialModule } from '../../helpers/shared-material.module';
 
@@ -9,7 +10,8 @@ import { LeaderboardComponent } from './leaderboard.component';
   declarations: [LeaderboardComponent],
   imports: [
     CommonModule,
-    SharedMaterialModule
+    SharedMaterialModule,
+    RouterModule
   ],
   exports: [LeaderboardComponent]
 })
--- src/app/_shared/components/notifications/notifications.module.ts
+++ src/app/_shared/components/notifications/notifications.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { NotificationsComponent } from './notifications.component';
 
 @NgModule({
   declarations: [NotificationsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [NotificationsComponent]
 })
