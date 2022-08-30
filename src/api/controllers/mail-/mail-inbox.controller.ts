c58355371df495fee362689d08becb3aa4f2f943
Bifeldy <bias.astho@gmail.com>
2022-08-31 02:25:14 +0900

    ✔ Email Inbox 🎉 ✨

--- src/api/controllers/mail-/mail-inbox.controller.ts
+++ src/api/controllers/mail-/mail-inbox.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpCode, HttpException, HttpStatus, Get, Req, Res } from '@nestjs/common';
-import { ILike } from 'typeorm';
+import { Equal, ILike } from 'typeorm';
 import { Request, Response } from 'express';
 
 import { environment } from '../../../environments/api/environment';
@@ -51,6 +51,16 @@ export class MailInboxController {
         skip: queryPage > 0 ? (queryPage * queryRow - queryRow) : 0,
         take: (queryRow > 0 && queryRow <= 500) ? queryRow : 10
       });
+      for (const m of mailboxs) {
+        delete m.html;
+        delete m.text;
+        if ('attachment_' in m && m.attachment_) {
+          for (const a of m.attachment_) {
+            delete a.created_at;
+            delete a.updated_at;
+          }
+        }
+      }
       return {
         info: '😍 200 - Mail Inbox API :: All Email 🥰',
         count,
@@ -65,6 +75,53 @@ export class MailInboxController {
           message: 'Data Tidak Lengkap!'
         }
       }, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Get('/:id')
+  @HttpCode(200)
+  @Roles(RoleModel.ADMIN, RoleModel.MODERATOR, RoleModel.FANSUBBER, RoleModel.USER)
+  @VerifiedOnly()
+  async getById(@Req() req: Request, @Res({ passthrough: true }) res: Response): Promise<any> {
+    try {
+      const user: UserModel = res.locals['user'];
+      const mailbox = await this.mailboxRepo.findOneOrFail({
+        where: [
+          { id: Equal(req.params['id']) }
+        ],
+        relations: ['attachment_'],
+      });
+      if (
+        !mailbox.from.includes(`${user.username}@${environment.domain}`) &&
+        !mailbox.to.includes(`${user.username}@${environment.domain}`) &&
+        !mailbox.cc.includes(`${user.username}@${environment.domain}`) &&
+        !mailbox.bcc.includes(`${user.username}@${environment.domain}`)
+      ) {
+        throw new HttpException({
+          info: `🙄 400 - Mail Inbox API :: Gagal Mendapatkan Email 😪`,
+          result: {
+            message: 'Email Milik Orang Lain!'
+          }
+        }, HttpStatus.FORBIDDEN);
+      }
+      if ('attachment_' in mailbox && mailbox.attachment_) {
+        for (const a of mailbox.attachment_) {
+          delete a.created_at;
+          delete a.updated_at;
+        }
+      }
+      return {
+        info: `😅 200 - Mail Inbox API :: Detail ${req.params['id']} 🤣`,
+        result: mailbox
+      };
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+      throw new HttpException({
+        info: `🙄 404 - Mail Inbox API :: Gagal Mencari Email ${req.params['id']} 😪`,
+        result: {
+          message: 'Email Tidak Ditemukan!'
+        }
+      }, HttpStatus.NOT_FOUND);
     }
   }
 
