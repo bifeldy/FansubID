9f9b9a7b8f08fedca232e457075869578a3c0d59
Bifeldy <bias.astho@gmail.com>
2022-08-31 17:51:45 +0900

    ✔ ✨ Prepare Surel~ 🎉

---
+++ src/app/_pages/mailbox/mailbox-detail/mailbox-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mailbox-detail',
+  templateUrl: './mailbox-detail.component.html',
+  styleUrls: ['./mailbox-detail.component.css']
+})
+export class MailboxDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/_pages/mailbox/mailbox.module.ts
+++ src/app/_pages/mailbox/mailbox.module.ts
@@ -5,21 +5,42 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SharedMaterialModule } from '../../_shared/modules/shared-material.module';
 
-import { MailboxListComponent } from './mailbox-list/mailbox-list.component';
 import { MailboxComposeComponent } from './mailbox-compose/mailbox-compose.component';
+import { MailboxDetailComponent } from './mailbox-detail/mailbox-detail.component';
+import { MailboxListComponent } from './mailbox-list/mailbox-list.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: MailboxListComponent
+  },
+  {
+    path: 'compose',
+    component: MailboxComposeComponent,
+    data: {
+      title: 'Surel - Buat Baru',
+      description: 'Halaman Kirim Surel Baru',
+      keywords: 'Buat Surel Baru'
+    }
+  },
+  {
+    path: ':mailId',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: MailboxDetailComponent
+      }
+    ]
   }
 ];
 
 @NgModule({
   declarations: [
     MailboxListComponent,
-    MailboxComposeComponent
+    MailboxComposeComponent,
+    MailboxDetailComponent
   ],
   imports: [
     CommonModule,
