5f3cf9d1edc5980c527f8b7722cb214d895d0e6f
Bifeldy <bifeldy@gmail.com>
2023-08-02 07:30:43 +0900

    ✔ Fix Click ~

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -271,24 +271,26 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
     this.gs.log('[MOUSE_LEFT_CLICK]', ev);
     const e = ev || window.event;
     let el = e.target || e.srcElement;
-    let maxLoop = 5;
-    let linkTag = ['A', 'a'];
-    while(![...linkTag].includes(el.tagName) && maxLoop > 0) {
-      el = el.parentElement || el.parentNode;
-      maxLoop--;
-    }
-    if (linkTag.includes(el.tagName)) {
-      const externalUri: string = el.getAttribute('href');
-      if (externalUri) {
-        if (
-          this.gs.isDesktop &&
-          this.gs.includesOneOf(externalUri, ['http', 'ftp', 'mailto']) &&
-          !externalUri.includes(environment.baseUrl)
-        ) {
-          e.preventDefault();
-          e.stopPropagation();
-          this.winboxOpenUri(externalUri);
-          return false;
+    if (el) {
+      let maxLoop = 5;
+      const linkTag = ['A', 'a'];
+      while(![...linkTag].includes(el.tagName) && maxLoop > 0) {
+        el = el.parentElement || el.parentNode;
+        maxLoop--;
+      }
+      if (linkTag.includes(el.tagName)) {
+        const externalUri: string = el.getAttribute('href');
+        if (externalUri) {
+          if (
+            this.gs.isDesktop &&
+            this.gs.includesOneOf(externalUri, ['http', 'ftp', 'mailto']) &&
+            !externalUri.includes(environment.baseUrl)
+          ) {
+            e.preventDefault();
+            e.stopPropagation();
+            this.winboxOpenUri(externalUri);
+            return false;
+          }
         }
       }
     }
