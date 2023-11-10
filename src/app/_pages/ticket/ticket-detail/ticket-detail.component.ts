045544ab8051d50d64a69ea65040296aa6c81a8f
Bifeldy <bifeldy@gmail.com>
2023-11-11 03:29:14 +0900

    ✔ Ticket ~

--- src/app/_pages/ticket/ticket-detail/ticket-detail.component.ts
+++ src/app/_pages/ticket/ticket-detail/ticket-detail.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { environment } from '../../../../environments/app/environment';
+
 import { GlobalService } from '../../../_shared/services/global.service';
 import { TicketService } from '../../../_shared/services/ticket.service';
 import { PageInfoService } from '../../../_shared/services/page-info.service';
 import { BusyService } from '../../../_shared/services/busy.service';
+import { DialogService } from '../../../_shared/services/dialog.service';
 
 @Component({
   selector: 'app-ticket-detail',
@@ -20,6 +23,7 @@ export class TicketDetailComponent implements OnInit, OnDestroy {
   subsActRoute = null;
   subsTicket = null;
   subsParam = null;
+  subsDialog = null;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -27,7 +31,8 @@ export class TicketDetailComponent implements OnInit, OnDestroy {
     private bs: BusyService,
     private gs: GlobalService,
     private pi: PageInfoService,
-    private ticket: TicketService
+    private ticket: TicketService,
+    private ds: DialogService
   ) {
     this.gs.bannerImg = null;
     this.gs.sizeContain = false;
@@ -38,12 +43,41 @@ export class TicketDetailComponent implements OnInit, OnDestroy {
     this.subsActRoute?.unsubscribe();
     this.subsTicket?.unsubscribe();
     this.subsParam?.unsubscribe();
+    this.subsDialog?.unsubscribe();
   }
 
   ngOnInit(): void {
-    this.ticketId = Number(this.activatedRoute.snapshot.paramMap.get('ticketId') || '');
-    this.ticketSecret = this.activatedRoute.snapshot.queryParamMap.get('secret') || '';
-    this.getTicket();
+    if (this.gs.isBrowser) {
+      this.ticketId = Number(this.activatedRoute.snapshot.paramMap.get('ticketId') || '');
+      this.ticketSecret = this.activatedRoute.snapshot.queryParamMap.get('secret') || '';
+      this.promptSecret();
+    }
+  }
+
+  promptSecret(): void {
+    this.subsDialog = this.ds.openInputDialog({
+      data: {
+        title: 'Masukkan Kata Sandi Secret Key',
+        input: {
+          secret: {
+            inputLabel: 'Secret',
+            inputPlaceholder: environment.apiKey,
+            inputValue: this.ticketSecret,
+            inputRequired: true
+          }
+        },
+        confirmText: 'OK'
+      }
+    }).afterClosed().subscribe({
+      next: re => {
+        this.gs.log('[INPUT_DIALOG_CLOSED]', re);
+        if (re) {
+          this.ticketSecret = re.secret;
+        }
+        this.getTicket();
+        this.subsDialog.unsubscribe();
+      }
+    });
   }
 
   getTicket(): void {
