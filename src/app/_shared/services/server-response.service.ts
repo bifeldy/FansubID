8662e1a7bef82a7156cbcf1bcefd7b530e421fc6
Bifeldy <bifeldy@gmail.com>
2023-11-16 00:27:08 +0900

    ✔ 404 Token ~

--- src/app/_shared/services/server-response.service.ts
+++ src/app/_shared/services/server-response.service.ts
@@ -1,5 +1,6 @@
-import { RESPONSE } from '@nguniversal/express-engine/tokens';
 import { Inject, Injectable, Optional } from '@angular/core';
+import { RESPONSE } from '@nestjs/ng-universal/dist/tokens';
+
 import { Response } from 'express';
 
 import { GlobalService } from './global.service';
