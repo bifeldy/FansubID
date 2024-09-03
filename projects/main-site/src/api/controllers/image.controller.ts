ad6cdd0716228bbbc5b6729630d75c99f6e0b63a
Bifeldy <bifeldy@gmail.com>
2024-09-04 02:26:14 +0900

    ✔ Fix Gambar ~

--- projects/main-site/src/api/controllers/image.controller.ts
+++ projects/main-site/src/api/controllers/image.controller.ts
@@ -103,7 +103,7 @@ export class ImageController {
         form.append('key', environment.imgbbKey);
         form.append('name', dateTime);
         form.append('image', imgB64);
-        const res_raw = await this.api.postData(url, form, environment.nodeJsXhrHeader, res.locals['abort-controller'].signal);
+        const res_raw = await this.api.postData(url, form, environment.nodeJsXhrHeader);
         if (res_raw.ok) {
           const res_json: any = await res_raw.json();
           this.gs.log(`[imgBB] 🖼 ${res_raw.status}`, res_json);
