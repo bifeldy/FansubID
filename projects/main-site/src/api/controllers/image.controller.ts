8e450738a706eb509a20179509203c1ca71b618a
Bifeldy <bifeldy@gmail.com>
2024-09-03 11:42:47 +0900

    ✔ Prime Account ~

--- projects/main-site/src/api/controllers/image.controller.ts
+++ projects/main-site/src/api/controllers/image.controller.ts
@@ -9,6 +9,7 @@ import { Controller, HttpCode, HttpException, HttpStatus, Post, Req, Res, UseInt
 import { ApiExcludeController } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Request, Response } from 'express';
+import { Equal, MoreThanOrEqual } from 'typeorm';
 
 import { CONSTANTS } from '../../constants';
 
@@ -23,6 +24,7 @@ import { ApiService } from '../services/api.service';
 import { CryptoService } from '../services/crypto.service';
 import { GlobalService } from '../services/global.service';
 import { AmazonWebService } from '../services/amazon-web.service';
+import { UserPremiumService } from '../repository/user-premium.service';
 
 @ApiExcludeController()
 @Controller('/image')
@@ -32,7 +34,8 @@ export class ImageController {
     private api: ApiService,
     private cs: CryptoService,
     private gs: GlobalService,
-    private aws: AmazonWebService
+    private aws: AmazonWebService,
+    private userPremiumRepo: UserPremiumService
   ) {
     //
   }
@@ -64,55 +67,65 @@ export class ImageController {
   )
   async imgBb(@Req() req: Request, @Res({ passthrough: true }) res: Response): Promise<any> {
     try {
+      const user: UserModel = res.locals['user'];
+      const primeCount = await this.userPremiumRepo.count({
+        where: [
+          {
+            expired_at: MoreThanOrEqual(new Date()),
+            user_: {
+              id: Equal(user.id)
+            }
+          }
+        ],
+        relations: ['user_']
+      });
       const dateTime = Date.now().toString();
       const imgB64 = this.cs.convertToBase64(req.file.buffer);
-      const url = new URL(environment.externalApiImage);
-      const form = new FormData();
-      form.append('key', environment.imgbbKey);
-      form.append('name', dateTime);
-      form.append('image', imgB64);
-      const res_raw = await this.api.postData(url, form, environment.nodeJsXhrHeader, res.locals['abort-controller'].signal);
-      if (res_raw.ok) {
-        const res_json: any = await res_raw.json();
-        this.gs.log(`[imgBB] 🖼 ${res_raw.status}`, res_json);
+      if (primeCount > 0 && user.id !== 2 /* TODO :: Hard-coded Bot 'Backup' Account Prevent DDL */) {
+        const upload = await this.aws.uploadImage(
+          `u${user.id}/img/${dateTime}`,
+          imgB64,
+          req.file.mimetype
+        );
         return {
-          info: `😅 201 - ImgBB S3 API :: Upload Image 🤣`,
+          info: `😅 201 - Gambar API :: Upload Image 🤣`,
           result: {
-            id: res_json.data.id,
-            url: res_json.data.image.url,
-            mime: res_json.data.image.mime,
-            size: res_json.data.size,
-            // title: res_json.data.title,
-            // extension: res_json.data.image.extension,
-            // time: res_json.data.time,
-            // expiration: res_json.data.expiration
+            id: upload.Key,
+            url: upload.Location,
+            mime: req.file.mimetype,
+            size: req.file.size
           },
-          imageUrl: res_json.data.image.url
+          imageUrl: upload.Location
         };
       } else {
-        try {
-          const user: UserModel = res.locals['user'];
-          const upload = await this.aws.uploadImage(
-            `u${user.id}/img/${dateTime}`,
-            imgB64,
-            req.file.mimetype
-          );
-          this.gs.log(`[awsS3] 🖼 ${res_raw.status}`, upload);
+        const url = new URL(environment.externalApiImage);
+        const form = new FormData();
+        form.append('key', environment.imgbbKey);
+        form.append('name', dateTime);
+        form.append('image', imgB64);
+        const res_raw = await this.api.postData(url, form, environment.nodeJsXhrHeader, res.locals['abort-controller'].signal);
+        if (res_raw.ok) {
+          const res_json: any = await res_raw.json();
+          this.gs.log(`[imgBB] 🖼 ${res_raw.status}`, res_json);
           return {
-            info: `😅 201 - ImgBB S3 API :: Upload Image 🤣`,
+            info: `😅 201 - Gambar API :: Upload Image 🤣`,
             result: {
-              id: upload.Key,
-              url: upload.Location,
-              mime: req.file.mimetype,
-              size: req.file.size
+              id: res_json.data.id,
+              url: res_json.data.image.url,
+              mime: res_json.data.image.mime,
+              size: res_json.data.size,
+              // title: res_json.data.title,
+              // extension: res_json.data.image.extension,
+              // time: res_json.data.time,
+              // expiration: res_json.data.expiration
             },
-            imageUrl: upload.Location
+            imageUrl: res_json.data.image.url
           };
-        } catch (e) {
+        } else {
           throw new HttpException({
-            info: `🙄 ${res_raw.status || 400} - ImgBB S3 API :: Upload Image Gagal 😪`,
+            info: `🙄 ${res_raw.status || 400} - Gambar API :: Upload Image Gagal 😪`,
             result: {
-              message: 'Data Tidak Lengkap / ImgBB S3 API Down!'
+              message: 'Data Tidak Lengkap / Gambar API Down!'
             }
           }, res_raw.status || HttpStatus.BAD_REQUEST);
         }
@@ -120,7 +133,7 @@ export class ImageController {
     } catch (error) {
       if (error instanceof HttpException) throw error;
       throw new HttpException({
-        info: `🙄 400 - ImgBB S3 API :: Upload Image Gagal 😪`,
+        info: `🙄 400 - Gambar API :: Upload Image Gagal 😪`,
         result: {
           message: 'Data Tidak Lengkap'
         }
