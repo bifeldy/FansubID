2699c6d19ccb4d9e8a0352bea237bbfd5395d4a6
Bifeldy <bifeldy@gmail.com>
2024-09-14 11:56:53 +0900

    ✔ env

---
+++ projects/alt-site/src/environment.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  siteName: 'FanshareID',
+  siteDescription: 'Berbagi Pakai File Gratis!',
+  author: 'Bifeldy',
+  domain: 'fanshare.id',
+  baseUrl: 'https://www.fanshare.id',
+  apiUrl: 'https://www.fansub.id/api',
+  apiKey: '00000000-0000-0000-0000-000000000000',
+  discord: {
+    client_id: '789831990433153034',
+    join_url: 'https://discord.gg/xGWdExk',
+    guild_id: '342220398022098944'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+
+// import 'zone.js/plugins/zone-error';
+// Included with Angular CLI.
