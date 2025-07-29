a3fa4f0381a3f5b1581b124fccde8b921ef2c2be
Bifeldy <bifeldy@gmail.com>
2025-07-29 22:51:24 +0900

    ✔ Torrent Scrape ~

--- projects/main-site/src/api/controllers/torrent.controller.ts
+++ projects/main-site/src/api/controllers/torrent.controller.ts
@@ -1,8 +1,9 @@
 // 3rd Party Library
-import webtorrentHealth from 'webtorrent-health';
+import bittorrentTrackerClient from 'bittorrent-tracker/client';
 
 import { Controller, HttpCode, HttpStatus, Post, Req, Res } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
+import { SchedulerRegistry } from '@nestjs/schedule';
 import { Request, Response } from 'express';
 import { classToPlain } from 'class-transformer';
 
@@ -10,11 +11,61 @@ import { environment } from '../../environments/api/environment';
 
 import { FilterApiKeyAccess } from '../decorators/filter-api-key-access.decorator';
 
+import { GlobalService } from '../services/global.service';
+
 @ApiExcludeController()
 @Controller('/torrent')
 export class TorrentController {
 
-  constructor() { }
+  constructor(
+    private sr: SchedulerRegistry,
+    private gs: GlobalService
+  ) {
+    //
+  }
+
+  scrapeTorrent(announce: string, infoHash: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.sr.addTimeout(
+        `${infoHash}_${announce}`,
+        setTimeout(() => {
+          try {
+            reject('Request Timeout!');
+          } catch (e) {
+            this.gs.log('[TORRENT_SCRAPE-TIMEOUT] 👣', e, 'error');
+          }
+        }, 2345)
+      );
+      bittorrentTrackerClient.scrape(
+        {
+          announce,
+          infoHash
+        },
+        (err, result) => {
+          try {
+            this.sr.deleteTimeout(`${infoHash}_${announce}`);
+            const data = {
+              announce,
+              seeds: 0,
+              peers: 0,
+              downloads: 0
+            };
+            if (!err) {
+              // data.announce = result.announce;
+              data.seeds = result.complete;
+              data.peers = result.incomplete;
+              data.downloads = result.downloaded;
+              resolve(data);
+            } else {
+              reject(err.message);
+            }
+          } catch (e) {
+            this.gs.log('[TORRENT_SCRAPE-ERROR] 👣', e, 'error');
+          }
+        }
+      );
+    });
+  }
 
   @Post('/')
   @HttpCode(201)
@@ -22,24 +73,32 @@ export class TorrentController {
   async addNew(@Req() req: Request, @Res(/* { passthrough: true } */) res: Response): Promise<any> {
     try {
       if ('magnetHash' in req.body) {
-        webtorrentHealth(req.body.magnetHash, {
-          trackers: req.body.trackList || environment.torrent.trackerAnnounce,
-          timeout: req.body.trackTimeout || 1234
-        }, (err, data) => {
-          if (err) {
-            res.status(HttpStatus.BAD_REQUEST).json(classToPlain({
-              info: `🙄 400 - Torrent Tracker API :: Gagal Mendapatkan Torrent 😪`,
-              result: {
-                message: err.message
-              }
-            }));
-          } else {
-            res.status(HttpStatus.CREATED).json(classToPlain({
-              info: `😅 200 - Torrent Tracker API :: Berhasil Mendapatkan ${req.body.magnetHash} 🤣`,
-              result: data
-            }));
+        const torrentTracker = {
+          infoHash: req.body.magnetHash,
+          seeds: 0,
+          peers: 0,
+          downloads: 0,
+          trackers: []
+        };
+        const trackers = req.body.trackList || environment.torrent.trackerAnnounce;
+        for (const tracker of trackers) {
+          try {
+            const data = await this.scrapeTorrent(tracker,req.body.magnetHash);
+            torrentTracker.seeds += data.seeds;
+            torrentTracker.peers += data.peers;
+            torrentTracker.downloads += data.downloads;
+            torrentTracker.trackers.push(data);
+          } catch (e) {
+            torrentTracker.trackers.push({
+              announce: tracker,
+              error: e
+            });
           }
-        });
+        }
+        res.status(HttpStatus.CREATED).json(classToPlain({
+          info: `😅 200 - Torrent Tracker API :: ${req.body.magnetHash} 🤣`,
+          result: torrentTracker
+        }));
       } else {
         throw new Error('Data Tidak Lengkap!');
       }
