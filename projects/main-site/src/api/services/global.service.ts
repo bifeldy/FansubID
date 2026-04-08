ed57ae8c25b5a138ffe69755f61c580cb15f90fb
Basilius Bias Astho Christyono <bias.astho@gmail.com>
2026-04-08 10:56:48 +0700

    ✔ toISOStringWithTZ

--- projects/main-site/src/api/services/global.service.ts
+++ projects/main-site/src/api/services/global.service.ts
@@ -187,4 +187,19 @@ export class GlobalService {
     return `${value} ${unit[i] || '??'}`;
   }
 
+  toISOStringWithTZ(date: Date): string {
+    const pad = (num: number) => num.toString().padStart(2, '0');
+    
+    const tzOffset = -date.getTimezoneOffset();
+    const diff = tzOffset >= 0 ? '+' : '-';
+    const tzFormatted = diff + pad(Math.floor(Math.abs(tzOffset) / 60)) + ':' + pad(Math.abs(tzOffset) % 60);
+  
+    return [
+      date.getFullYear(), '-', pad(date.getMonth() + 1), '-', pad(date.getDate()),
+      'T', pad(date.getHours()), ':', pad(date.getMinutes()), ':', pad(date.getSeconds()),
+      '.', (date.getMilliseconds() / 1000).toFixed(3).slice(2, 5),
+      tzFormatted
+    ].join('');
+  }
+
 }
